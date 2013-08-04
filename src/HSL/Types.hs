3a3130b220b1d2de745c3b7c287d193f87db4772
Scott Sadler <github@mashi.org>
2013-08-04 18:10:23 +0100

    Use Data.Text everywhere

--- src/HSL/Types.hs
+++ src/HSL/Types.hs
@@ -2,15 +2,15 @@
 
 module HSL.Types where
 
-import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Text as T
 import Data.List (intersperse)
 import Data.Int (Int64)
 
 
 -- Type witnesses
 
-s :: B.ByteString
-s = undefined
+t :: T.Text
+t = undefined
 
 i :: Int
 i = undefined
@@ -22,30 +22,26 @@ f = undefined
 -- Output Rendering
 
 class Datum a where
-    bs :: a -> B.ByteString
+    bs :: a -> T.Text
 
 class Renderable a where
-    render :: a -> [B.ByteString]
+    render :: a -> [T.Text]
 
 
-instance Datum a => Datum (Maybe a) where
-    bs (Just a) = B.concat ["Just ", bs a]
-    bs Nothing = "Nothing"
-
-instance Datum Int where bs = B.pack . show
-instance Datum Int64 where bs = B.pack . show
-instance Datum Char where bs = B.pack . show
-instance Datum String where bs = B.pack
-instance Datum B.ByteString where bs = id
+instance Datum Int where bs = T.pack . show
+instance Datum Int64 where bs = T.pack . show
+instance Datum Char where bs = T.pack . show
+instance Datum String where bs = T.pack
+instance Datum T.Text where bs = id
 
 instance (Datum a, Datum b) => Datum (a, b) where
-    bs (a, b) = B.concat $ intersperse "\t" [bs a, bs b]
+    bs (a, b) = T.concat $ intersperse "\t" [bs a, bs b]
 instance (Datum a, Datum b, Datum c) => Datum (a, b, c) where
-    bs (a, b, c) = B.concat $ intersperse "\t" [bs a, bs b, bs c]
+    bs (a, b, c) = T.concat $ intersperse "\t" [bs a, bs b, bs c]
 instance (Datum a, Datum b, Datum c, Datum d) => Datum (a, b, c, d) where
-    bs (a, b, c, d) = B.concat $ intersperse "\t" [bs a, bs b, bs c, bs d]
+    bs (a, b, c, d) = T.concat $ intersperse "\t" [bs a, bs b, bs c, bs d]
 instance (Datum a, Datum b, Datum c, Datum d, Datum e) => Datum (a, b, c, d, e) where
-    bs (a, b, c, d, e) = B.concat $ intersperse "\t" [bs a, bs b, bs c, bs d, bs e]
+    bs (a, b, c, d, e) = T.concat $ intersperse "\t" [bs a, bs b, bs c, bs d, bs e]
 
 instance (Datum a) => Renderable a where
     render = (:[]) . bs
--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,7 @@
 import           Prelude hiding (break)
 
 import           Control.Category ((>>>))
-import           Control.Lens
-import           Data.ByteString.Lazy.Char8 (ByteString, break)
-import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Text as T
 import           Data.Char
 import           Data.List hiding (lines, unlines, break)
 import           Data.Maybe
@@ -14,15 +12,14 @@ import           Data.Ord
 import           Data.Tuple
 
 import           HSL.Json
-import           HSL.Types
 import           HSL.Stdlib
+import           HSL.IO
+import           HSL.Types
 
 
 %s
 
 p = %s
 
-run :: Renderable a => ([ByteString] -> a) -> IO ()
-run f = B.getContents >>= mapM_ B.putStrLn . render . f . B.lines
+main = runLineProcessor p
 
-main = run p
